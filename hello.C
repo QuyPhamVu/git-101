c7a95f5bd5885f8c0f53e2b1ed0f6c867ffe3a2d
Dat Pham <datpham@Dats-MacBook-Pro.local>
2024-11-19 22:58:44 +0700

    update forr loop

--- hello.C
+++ hello.C
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main() {
    printf("Hello, FTU");
+   
+   int i;
+   printf("==============");
+   for (i = 0; i < 5; i++) {
+      printf("%d\n", i);
+   }
+   printf("==============");
+   
    return 0;
 }
+
